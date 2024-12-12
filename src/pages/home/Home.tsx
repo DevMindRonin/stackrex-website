e8330f7908f790902aba5f18059d7cfd98fe1343
Jiri Groulik <devmindronin@gmail.com>
2024-12-12 15:32:58 +0100

    correction before build

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,7 +3,7 @@ import Hero from "../../sections/Hero";
 import Numbers from "../../sections/Numbers";
 import Main from "./../../sections/Main";
 import Contact from "../../sections/Contacts";
-import RunningText from "../../components/RunningText";
+
 const App = () => {
   return (
     <>
